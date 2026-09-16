b4a8dc16e7c59a9a6f09fef6100894695ee39341
Ryan-Martin-318 <rm1027324@otc.edu>
2026-09-16 09:37:56 -0500

    _

---
+++ form.php
@@ -0,0 +1,5 @@
+<form action="index.php" method="get">
+    <input type="date" name="Return_Date" placeholder="The date of the book that will be returned"/>
+    <input type="date" name="Due_Date" placeholder="The date the book is due"/>
+    <input type="submit"/>
+</form>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+    include("functions.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Library Book Date Checker</title>
+</head>
+<body>
+    <?php include("form.php"); ?>
+    <
+</body>
+</html>
